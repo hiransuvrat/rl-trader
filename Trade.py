b93dcd601cd90b1a7437b036380378c978361304
suvrat <hiran.suvrat@gmail.com>
2016-10-07 16:23:28 +0530

    bug

--- Trade.py
+++ Trade.py
@@ -3,7 +3,8 @@ __author__ = 'suvrat'
 from ModelBuilder import Model
 from DataFetcher import DataFetcher
 
-dataFetcher = DataFetcher('data/nifty_1500.csv').initProcessing('data/train.pkl', 'data/test.pkl')
+dataFetcher = DataFetcher('data/nifty_1500.csv')
+dataFetcher.initProcessing('data/train.pkl', 'data/test.pkl')
 modelBuilder = Model(dataFetcher)
 modelBuilder.setModel()
 modelBuilder.trainModel()

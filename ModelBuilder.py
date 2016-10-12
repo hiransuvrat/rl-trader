1f03bf41878a4f0923ad9017afbdd707062a898b
suvrat <hiran.suvrat@gmail.com>
2016-10-12 16:26:56 +0530

    bug

--- ModelBuilder.py
+++ ModelBuilder.py
@@ -124,6 +124,7 @@ class Model:
                             update = (reward + (self.gamma * maxQ))
                         else: #terminal state
                             update = reward
+                        print update
                         y[0][action] = update
                         #print(time_step, reward, terminal_state)
                         X_train.append(old_state)

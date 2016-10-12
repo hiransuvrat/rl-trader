6c0f6a9de737c52151412b8008d3b7b078950b14
suvrat <hiran.suvrat@gmail.com>
2016-10-12 12:26:53 +0530

    bug

--- Trade.py
+++ Trade.py
@@ -3,7 +3,8 @@ __author__ = 'suvrat'
 from ModelBuilder import Model
 from DataFetcher import DataFetcher
 
+inputFile = 'data/nifty_1500.csv'
 dataFetcher = DataFetcher('data/nifty_1500.csv', 'data/train.pkl', 'data/test.pkl', splitPercentage=.8)
-modelBuilder = Model(dataFetcher)
+modelBuilder = Model(inputFile)
 modelBuilder.setModel()
 modelBuilder.trainModel()
